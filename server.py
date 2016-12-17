561db59a9382d7c2ae0d40cb25bc33937254320b
Hamid Zare <dellydela@gmail.com>
2016-12-17 21:43:28 +0300

    added an example

--- server.py
+++ server.py
@@ -1,1 +1,75 @@
-ss
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+# """
+# This Bot uses the Updater class to handle the bot.
+# First, a few handler functions are defined. Then, those functions are passed to
+# the Dispatcher and registered at their respective places.
+# Then, the bot is started and runs until we press Ctrl-C on the command line.
+# Usage:
+# Basic Echobot example, repeats messages.
+# Press Ctrl-C on the command line or send a signal to the process to stop the
+# bot.
+# """
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    update.message.reply_text('Help!')
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+def base_logic(bot, update):
+	update.message.reply_text(update.message.text)
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("244680876:AAHJYczo4r_RmC20LsRr7_BMFNgGRm_UB3k")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # base logic
+    dp.add_handler(MessageHandler(Filters.text, base_logic))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
